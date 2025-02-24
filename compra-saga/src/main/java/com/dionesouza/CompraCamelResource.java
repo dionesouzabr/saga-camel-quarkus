1059b896ca6e1ddeb3e88330052611aa777d86a2
Dione de Souza Silva <dione2508@gmail.com>
2025-02-24 13:21:17 -0300

    Saga na mesma aplicação.

---
+++ compra-saga/src/main/java/com/dionesouza/CompraCamelResource.java
@@ -0,0 +1,52 @@
+package com.dionesouza;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.camel.CamelContext;
+
+@Path("compra-camel")
+public class CompraCamelResource {
+
+    @Inject
+    CamelContext context;
+
+    @Path("teste")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response saga(){
+
+        try{
+            Long id = 0L;
+
+            comprar(++id, 20);
+            comprar(++id, 30);
+            comprar(++id, 30);
+            comprar(++id, 25);
+
+            return Response.ok().build();
+
+        } catch (Exception e){
+            return Response.status(500).build();
+        }
+    }
+
+    private void comprar(Long id, int valor){
+        System.out.println("Pedido: " + id + " valor: " + valor);
+
+        try{
+            context.createFluentProducerTemplate()
+                    .to("direct:saga")
+                    .withHeader("id", id)
+                    .withHeader("valor", valor)
+                    .request();
+        } catch(Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+}
---
+++ compra-saga/src/main/java/com/dionesouza/PedidoService.java
@@ -0,0 +1,22 @@
+package com.dionesouza;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.Header;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationScoped
+public class PedidoService {
+
+
+    private Set<Long> pedidos = new HashSet<>();
+
+    public void newPedido(@Header("id") Long id){
+        pedidos.add(id);
+    }
+
+    public void cancelPedido(@Header("id") Long id){
+        pedidos.remove(id);
+    }
+}
